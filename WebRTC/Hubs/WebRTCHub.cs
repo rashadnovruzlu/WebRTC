74b4385694ec0bcc79d78e7cdd24d46c7dbe363b
Rashad Novruzlu <Rashad.NY@bestcomp.net>
2020-10-07 17:46:31 +0400

    Add project files.

---
+++ WebRTC/Hubs/WebRTCHub.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebRTC
+{
+    public class TransferObject
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public object Data { get; set; }
+    }
+
+
+    public class WebRTCHub : Hub
+    {
+
+       static ConcurrentDictionary<string, string> _users = new ConcurrentDictionary<string, string>();
+
+
+        public async Task Join(string userName)
+        {
+            if (_users.FirstOrDefault(x => x.Key == userName).Key == null)
+            {
+                _users.TryAdd(userName, Context.ConnectionId);
+
+                await SendAsync(userName, new TransferObject { Type = "login", Data = true });
+
+            }
+            else
+            {
+                await SendAsync(userName, new TransferObject { Type = "login", Data = false });
+            }
+        }
+
+
+
+        public async Task SendMessage(TransferObject transferObject)
+        {
+            switch (transferObject.Type)
+            {
+                case "offer":
+                   
+                    var userName = _users.FirstOrDefault(x => x.Value == Context.ConnectionId).Key;
+                    
+                    await SendAsync(transferObject.Name, new TransferObject { Type = "offer", Data = transferObject.Data, Name = userName });
+                    
+                    break;
+              
+                case "answer":
+                    await SendAsync(transferObject.Name, new TransferObject { Type = "answer", Data = transferObject.Data });
+                   
+                    break;
+
+                case "candidate":
+
+                    await SendAsync(transferObject.Name, new TransferObject { Type = "candidate", Data = transferObject.Data });
+                  
+                    break;
+
+                default:
+                    break;
+            }
+
+
+        }
+
+        public async Task SendAsync(string userName, TransferObject transferObject)
+        {
+            string connectionId = _users[userName];
+
+            await Clients.Client(connectionId).SendAsync("ReceiveMessage", transferObject);
+        }
+    }
+}
---
+++ WebRTC/wwwroot/js/Client.js
@@ -0,0 +1,213 @@
+﻿var name;
+var connectedUser;
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/webRTCHub").build();
+
+connection.on("ReceiveMessage", function (response) {
+
+    console.log("Got message", response);
+
+    
+
+    switch (response.type) {
+        case "login":
+            handleLogin(response.data);
+            break;
+        //when somebody wants to call us 
+        case "offer":
+            handleOffer(response.data, response.name);
+            break;
+        case "answer":
+            handleAnswer(response.data);
+            break;
+        //when a remote peer sends an ice candidate to us 
+        case "candidate":
+            handleCandidate(response.data);
+            break;
+        case "leave":
+            handleLeave();
+            break;
+        default:
+            break;
+    } 
+});
+
+
+connection.start().then(function () {
+     
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+
+function send(message) {
+
+    message.name = connectedUser;
+
+    connection.invoke("SendMessage", message).catch(function (err) {
+        return console.error(err.toString());
+    });
+}
+
+function Join(userName) {
+
+    connection.invoke("Join", userName).catch(function (err) {
+        return console.error(err.toString());
+    });
+}
+//****** 
+//UI selectors block 
+//******
+
+var loginPage = document.querySelector('#loginPage');
+var usernameInput = document.querySelector('#usernameInput');
+var loginBtn = document.querySelector('#loginBtn');
+
+var callPage = document.querySelector('#callPage');
+var callToUsernameInput = document.querySelector('#callToUsernameInput');
+var callBtn = document.querySelector('#callBtn');
+
+var hangUpBtn = document.querySelector('#hangUpBtn');
+
+var localVideo = document.querySelector('#localVideo');
+var remoteVideo = document.querySelector('#remoteVideo');
+
+var yourConn;
+var stream;
+
+callPage.style.display = "none";
+
+// Login when the user clicks the button 
+loginBtn.addEventListener("click", function (event) {
+    name = usernameInput.value;
+
+    if (name.length > 0) {
+        Join(name);
+    }
+
+});
+
+function handleLogin(success) {
+    if (success === false) {
+        alert("Ooops...try a different username");
+    } else {
+        loginPage.style.display = "none";
+        callPage.style.display = "block";
+
+        //********************** 
+        //Starting a peer connection 
+        //********************** 
+
+        //getting local video stream 
+        navigator.webkitGetUserMedia({ video: true, audio: true }, function (myStream) {
+            stream = myStream;
+
+            //displaying local video stream on the page 
+           
+            try {
+                localVideo.srcObject = stream;
+            } catch (error) {
+                localVideo.src = window.URL.createObjectURL(stream);
+            }
+
+
+            //using Google public stun server 
+            var configuration = {
+                "iceServers": [{ "url": "stun:stun2.1.google.com:19302" }]
+            };
+
+            yourConn = new webkitRTCPeerConnection(configuration);
+
+            // setup stream listening 
+            yourConn.addStream(stream);
+
+            //when a remote user adds stream to the peer connection, we display it 
+            yourConn.onaddstream = function (e) {
+               
+                try {
+                    remoteVideo.srcObject = e.stream;
+                } catch (error) {
+                    remoteVideo.src = window.URL.createObjectURL(e.stream);
+                }
+            };
+
+            // Setup ice handling 
+            yourConn.onicecandidate = function (event) {
+                if (event.candidate) {
+                    send({
+                        type: "candidate",
+                        data: event.candidate
+                    });
+                }
+            };
+
+        }, function (error) {
+            console.log(error);
+        });
+
+    }
+};
+
+//initiating a call 
+callBtn.addEventListener("click", function () {
+    var callToUsername = callToUsernameInput.value;
+
+    if (callToUsername.length > 0) {
+
+        connectedUser = callToUsername;
+
+        // create an offer 
+        yourConn.createOffer(function (offer) {
+            send({
+                type: "offer",
+                data: offer
+            });
+
+            yourConn.setLocalDescription(offer);
+        }, function (error) {
+            alert("Error when creating an offer");
+        });
+
+    }
+});
+
+//when somebody sends us an offer 
+function handleOffer(offer, name) {
+    connectedUser = name;
+    yourConn.setRemoteDescription(new RTCSessionDescription(offer));
+
+    //create an answer to an offer 
+    yourConn.createAnswer(function (answer) {
+        yourConn.setLocalDescription(answer);
+
+        send({
+            type: "answer",
+            data: answer
+        });
+
+    }, function (error) {
+        alert("Error when creating an answer");
+    });
+};
+
+//when we got an answer from a remote user
+function handleAnswer(answer) {
+    yourConn.setRemoteDescription(new RTCSessionDescription(answer));
+};
+
+//when we got an ice candidate from a remote user 
+function handleCandidate(candidate) {
+    yourConn.addIceCandidate(new RTCIceCandidate(candidate));
+};
+
+//hang up 
+hangUpBtn.addEventListener("click", function () {
+
+    send({
+        type: "leave"
+    });
+
+    handleLeave();
+});
+
+ 
